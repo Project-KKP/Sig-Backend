60bd65328b91e64192ef1e3fb19f3d68727f8a6a
Hanif <algamarhanif@gmail.com>
2024-07-25 14:19:09 +0800

    update cors

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express')
+const express = require('express');
 const DataRoutes = require('./routes/DataRoutes');
 const errorHandler = require('./middleware/errorhandler');
-const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors'); // Importing cors
 
+const app = express();
+const port = 5000;
+
+// CORS options
+const corsOptions = {
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+};
+
+app.use(cors(corsOptions)); // Using cors with options
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', DataRoutes);
 
-app.use(cors()); // Using cors
 app.use(errorHandler);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
