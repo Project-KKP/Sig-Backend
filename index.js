e98e674f9be5b959107a08feeccfb6c4a8b82fa5
Hanif <algamarhanif@gmail.com>
2024-07-25 09:30:22 +0800

    add cors

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const errorHandler = require('./middleware/errorhandler');
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', DataRoutes);
 
+app.use(cors()); // Using cors
 app.use(errorHandler);
 
 
